0bb571f4079e8cb655fdce13fa5b8e1bc2429679
Risheit <80658104+Risheit@users.noreply.github.com>
2022-11-23 14:52:31 -0500

    Add verify implementation

--- app/src/main/java/com/presenters/UserManagement.java
+++ app/src/main/java/com/presenters/UserManagement.java
@@ -1,4 +1,15 @@
 package com.presenters;
 
+import com.models.UserDatabaseInterface;
+
+import java.util.Objects;
+
 public class UserManagement implements WriteUser, Verify {
+
+    @Override
+    public boolean verify(UserDatabaseInterface db, User user) {
+        User valid = db.getUser(user.getEmail());
+
+        return valid.getPassword().equals(user.getPassword());
+    }
 }
