dda81d70eebd07c198b117959f2072637dd3b178
Francis Gajitos <FrancisJed@outlook.com>
2022-11-22 23:37:46 -0500

    User database is finished

--- app/src/main/java/com/UserDatabase.java
+++ app/src/main/java/com/UserDatabase.java
@@ -1,16 +1,12 @@
 package com;
 
 import static android.content.ContentValues.TAG;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.presenters.User;
-
-
 import android.util.Log;
-
 import com.google.firebase.database.Query;
 import com.google.firebase.database.DatabaseReference;
 
