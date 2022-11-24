93c1ab4f0c01ab4b6651c0489578ba85780ce422
ExuApplePie <75225599+ExuApplePie@users.noreply.github.com>
2022-11-23 21:55:25 -0500

    Moved signup user to UserManagement

--- app/src/main/java/com/presenters/UserSignup.java
+++ app/src/main/java/com/presenters/UserSignup.java
@@ -2,27 +2,14 @@ package com.presenters;
 
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
-public class UserSignup {
+import com.models.UserDatabase;
+import com.models.UserDatabaseInterface;
 
-    public static void signupUser(EditText nameField, EditText emailField,
-                                  EditText passwordField, View view) {
-        User user = new User(nameField.getText().toString(), emailField.getText().toString(),
-                passwordField.getText().toString());
-
-        int duration = Toast.LENGTH_SHORT;
-
-        /*
-        if (UserManagement.writeUser(user)) {
-            // snackbar registration successful
-            CharSequence text = "Registriation Successful";
-        } else {
-            //snackbar registration failed
-            CharSequence text = "Registriation Failed";
-        }
-         */
-//        Toast toast = Toast.makeText(view.getContext(), text, duration);
-        //switch back to fragment 1 - responsibility for the calling method
-    }
+public interface UserSignup {
+    /***
+    Takes a object user of type User and a UserDatabaseInterface db; adds user to the database
+     if it does not exist already, returns true if user is added, false if not
+     */
+    boolean signupUser(User user, UserDatabaseInterface db);
 }
