3658b21b4e712e118aef419ac1de495705527a22
Risheit <80658104+Risheit@users.noreply.github.com>
2022-11-30 03:01:41 -0500

    Optimize UserDatabase imports

--- app/src/main/java/com/models/UserDatabase.java
+++ app/src/main/java/com/models/UserDatabase.java
@@ -1,18 +1,16 @@
 package com.models;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.presenters.users.User;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.google.firebase.database.Query;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.presenters.users.User;
 
 public class UserDatabase implements UserDatabaseInterface {
     private final DatabaseReference ref;
