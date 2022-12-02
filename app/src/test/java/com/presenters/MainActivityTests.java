0a408ee7a40f11dc0b973f9a679e0ecd123eba1d
Risheit <80658104+Risheit@users.noreply.github.com>
2022-12-02 05:16:26 -0500

    Add presenter tests

--- app/src/test/java/com/presenters/MainActivityTests.java
+++ app/src/test/java/com/presenters/MainActivityTests.java
@@ -1,8 +1,18 @@
 package com.presenters;
 
-import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import static org.junit.Assert.*;
+import android.text.Editable;
+import android.widget.EditText;
+
+import com.models.DatabaseMock;
+import com.models.users.User;
+import com.planner.MainActivity;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * MainActivity unit tests, which execute on the development machine (host).
@@ -10,8 +20,107 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class MainActivityTests {
+    private MainActivity view;
+    private MainActivityPresenter presenter;
+
+    private Editable emailReturn;
+    private Editable passwordReturn;
+
+    private User user1, user2;
+
+    @Before
+    public void setUp() {
+        // Initialize Mocks
+        DatabaseMock model = new DatabaseMock();
+        EditText emailInput = mock(EditText.class);
+        EditText passwordInput = mock(EditText.class);
+
+        view = mock(MainActivity.class);
+        emailReturn = mock(Editable.class);
+        passwordReturn = mock(Editable.class);
+
+        presenter = new MainActivityPresenter(view, model);
+
+        user1 = new User("Student", "Charles Mominjay", "charlesmj@gmail.com",
+                "password123");
+        user2 = new User("Admin", "Admin Man", "admin@adminmail.ca",
+                "ComplexAdminPassword");
+
+        model.addUser(user1);
+        model.addUser(user2);
+
+        // Setup Default Mocks
+        when(view.getEmailInput()).thenReturn(emailInput);
+        when(view.getPasswordInput()).thenReturn(passwordInput);
+        when(emailInput.getText()).thenReturn(emailReturn);
+        when(passwordInput.getText()).thenReturn(passwordReturn);
+    }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testLoginWithStudentLogin() {
+        // Setup Mocks
+        when(emailReturn.toString()).thenReturn(user1.getEmail());
+        when(passwordReturn.toString()).thenReturn(user1.getPassword());
+
+        presenter.onLoginButtonClicked();
+
+        // Check that Student Page opened
+        verify(view).openStudentHomepage(user1.getName());
+    }
+
+    @Test
+    public void testLoginWithAdminLogin() {
+        // Setup Mocks
+        when(emailReturn.toString()).thenReturn(user2.getEmail());
+        when(passwordReturn.toString()).thenReturn(user2.getPassword());
+
+        presenter.onLoginButtonClicked();
+
+        // Check that Admin Page opened
+        verify(view).openAdminHomepage(user2.getName());
+    }
+
+    @Test
+    public void testLoginWithInvalidEmail() {
+        // Setup Mocks
+        when(emailReturn.toString()).thenReturn("invalidEmail@mail");
+        when(passwordReturn.toString()).thenReturn(user1.getPassword());
+
+        presenter.onLoginButtonClicked();
+
+        // Check Error displayed
+        verify(view).displayErrorNotification("Invalid Email");
+    }
+
+    @Test
+    public void testLoginWithInvalidPassword() {
+        // Setup Mocks
+        when(emailReturn.toString()).thenReturn(user1.getEmail());
+        when(passwordReturn.toString()).thenReturn(user2.getPassword());
+
+        presenter.onLoginButtonClicked();
+
+        // Check Error displayed
+        verify(view).displayErrorNotification("Incorrect Password");
+    }
+
+    @Test
+    public void testLoginWithEmptyEmail() {
+        // Setup Mocks
+        when(emailReturn.toString()).thenReturn("");
+        when(passwordReturn.toString()).thenReturn(user1.getPassword());
+
+        presenter.onLoginButtonClicked();
+
+        // Check Error displayed
+        verify(view).displayErrorNotification("Please Enter All Possible Fields");
+    }
+
+    @Test
+    public void testSignUp() {
+        presenter.onSignUpButtonClicked();
+
+        // Check that Sign Up page opened
+        verify(view).openSignUpPage();
     }
 }
