51f2e33315498ab1d34f1965a6381bf096cf4699
Risheit <80658104+Risheit@users.noreply.github.com>
2022-12-01 20:14:16 -0500

    Add UserManagement tests

--- app/src/test/java/com/presenters/UserManagementTests.java
+++ app/src/test/java/com/presenters/UserManagementTests.java
@@ -1,83 +1,195 @@
-// TODO: Fix Tests
+package com.presenters;
 
-//package com.presenters;
-//
-//import com.models.UserDatabase;
-//import com.models.UserDatabaseInterface;
-//
-//import org.junit.*;
-//import static org.junit.Assert.*;
-//
-//public class UserManagementTests {
-//    private UserDatabaseInterface dbMock;
-//    private UserManagement management;
-//    private User user1;
-//
-//    @Before
-//    public void setUp() {
-//        dbMock = new DatabaseMock();
-//        management = new UserManagement(dbMock);
-//
-//        user1 = new User("student", "Charles Mominjay", "charlesmj@gmail.com",
-//                "password123");
-//
-//        dbMock.addUser(user1);
-//    }
-//
-//    @Test
-//    public void testVerifySuccess() {
-//        assertTrue(management.verify(user1));
-//    }
-//
-//    @Test
-//    public void testVerifyFailure() {
-//        User user = new User("student", "", "charlesmj@gmail.com", "");
-//        assertFalse(management.verify(user));
-//    }
-//
-//    @Test
-//    public void testVerifyWithNullPassword() {
-//        User user = new User("student", "", "charlesmj@gmail.com", null);
-//        assertFalse(management.verify(user));
-//    }
-//
-//    @Test
-//    public void testVerifyWithNullUser() {
-//        assertFalse(management.verify(null));
-//    }
-//
-//    @Test
-//    public void testSignupUserSuccess() {
-//        User user = new User("student", "test student", "newmail@mail.ca",
-//                "abc123");
-//        management.signupUser(user);
-//        assertEquals(user, dbMock.getUser(user.getEmail()));
-//    }
-//
-//    @Test
-//    public void testSignupUserSuccessReturnsTrue() {
-//        User user = new User("student", "test student", "newmail@mail.ca",
-//                "abc123");
-//        assertTrue(management.signupUser(user));
-//    }
-//
-//    @Test
-//    public void testSignupUserFails() {
-//        assertFalse(management.signupUser(user1));
-//    }
-//
-//    @Test
-//    public void testSignupUserSameEmail() {
-//        User user = new User("student", "test student", "charlesmj@gmail.com",
-//                "");
-//        management.signupUser(user);
-//        assertNotEquals(user, dbMock.getUser(user.getEmail()));
-//    }
-//
-//    @Test
-//    public void testSignupUserFailureReturnsFalse() {
-//        User user = new User("student", "test student", "charlesmj@gmail.com",
-//                "");
-//        assertFalse(management.signupUser(user));
-//    }
-//}
+import com.models.UserDatabaseInterface;
+import com.presenters.users.User;
+import com.presenters.users.UserLoginActions;
+import com.presenters.users.UserManagement;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class UserManagementTests {
+    private UserDatabaseInterface dbMock;
+    private UserManagement management;
+    private User user1, user2;
+
+    @Before
+    public void setUp() {
+        dbMock = new DatabaseMock();
+        management = new UserManagement(dbMock);
+
+        user1 = new User("Student", "Charles Mominjay", "charlesmj@gmail.com",
+                "password123");
+        user2 = new User("Admin", "Admin Man", "admin@adminmail.ca",
+                "ComplexAdminPassword");
+
+        dbMock.addUser(user1);
+        dbMock.addUser(user2);
+    }
+
+    @Test
+    public void testStudentLoginSuccess() {
+        management.login(user1.getEmail(), user1.getPassword(), new UserLoginActions() {
+            @Override
+            public void studentLoginSuccess(User user) {
+                assertEquals(user1, user);
+            }
+
+            @Override
+            public void adminLoginSuccess(User user) {
+                fail("Admin Login reached instead of Student Login.");
+            }
+
+            @Override
+            public void incorrectPassword(User user) {
+                fail("Incorrect password error received when correct password given.");
+            }
+
+            @Override
+            public void invalidEmail() {
+                fail("Invalid email error received when email is valid.");
+            }
+        });
+    }
+
+    @Test
+    public void testAdminLoginSuccess() {
+        management.login(user2.getEmail(), user2.getPassword(), new UserLoginActions() {
+            @Override
+            public void studentLoginSuccess(User user) {
+                fail("Student Login reached instead of Admin Login.");
+            }
+
+            @Override
+            public void adminLoginSuccess(User user) {
+                assertEquals(user2, user);
+            }
+
+            @Override
+            public void incorrectPassword(User user) {
+                fail("Incorrect password error received when correct password given.");
+            }
+
+            @Override
+            public void invalidEmail() {
+                fail("Invalid email error received when email is valid.");
+            }
+        });
+    }
+
+    @Test
+    public void testLoginWithIncorrectPassword() {
+        management.login(user1.getEmail(), "", new UserLoginActions() {
+            @Override
+            public void studentLoginSuccess(User user) {
+                fail("Student Login reached instead of receiving incorrect password error.");
+            }
+
+            @Override
+            public void adminLoginSuccess(User user) {
+                fail("Admin Login reached instead of receiving incorrect password error.");
+            }
+
+            @Override
+            public void incorrectPassword(User user) {
+                assertEquals(user1, user);
+            }
+
+            @Override
+            public void invalidEmail() {
+                fail("Invalid email error received when email is valid.");
+            }
+        });
+    }
+
+    @Test
+    public void testLoginWithNullPassword() {
+        management.login(user1.getEmail(), null, new UserLoginActions() {
+            @Override
+            public void studentLoginSuccess(User user) {
+                fail("Student Login reached instead of receiving incorrect password error.");
+            }
+
+            @Override
+            public void adminLoginSuccess(User user) {
+                fail("Admin Login reached instead of receiving incorrect password error.");
+            }
+
+            @Override
+            public void incorrectPassword(User user) {
+                assertEquals(user1, user);
+            }
+
+            @Override
+            public void invalidEmail() {
+                fail("Invalid email error received when email is valid.");
+            }
+        });
+    }
+
+    @Test
+    public void testLoginWithInvalidEmail() {
+        management.login("invalidEmail@bad.com", user1.getPassword(), new UserLoginActions() {
+            @Override
+            public void studentLoginSuccess(User user) {
+                fail("Student Login reached instead of receiving invalid email error.");
+            }
+
+            @Override
+            public void adminLoginSuccess(User user) {
+                fail("Admin Login reached instead of receiving invalid email error.");
+            }
+
+            @Override
+            public void incorrectPassword(User user) {
+                fail("Incorrect password error received instead of receiving invalid email error.");
+            }
+
+            @Override
+            public void invalidEmail() {
+                // Pass test
+            }
+        });
+    }
+
+    @Test
+    public void testLoginWithNullEmail() {
+        management.login(null, user1.getPassword(), new UserLoginActions() {
+            @Override
+            public void studentLoginSuccess(User user) {
+                fail("Student Login reached instead of receiving invalid email error.");
+            }
+
+            @Override
+            public void adminLoginSuccess(User user) {
+                fail("Admin Login reached instead of receiving invalid email error.");
+            }
+
+            @Override
+            public void incorrectPassword(User user) {
+                fail("Incorrect password error received instead of receiving invalid email error.");
+            }
+
+            @Override
+            public void invalidEmail() {
+                // Pass test
+            }
+        });
+    }
+
+    @Test
+    public void testSignupUserSuccess() {
+        User user = new User("student", "test student", "newmail@mail.ca",
+                "abc123");
+        management.signupUser(user);
+        assertEquals(user, ((DatabaseMock)dbMock).getUserSynchronous(user.getEmail()));
+    }
+
+    @Test
+    public void testSignupUserSameEmail() {
+        User user = new User("student", "test student", user1.getEmail(),
+                "");
+        management.signupUser(user);
+        assertNotEquals(user, ((DatabaseMock)dbMock).getUserSynchronous(user.getEmail()));
+    }
+}
