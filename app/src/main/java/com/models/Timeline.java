ad99742d2f0142f445ec1a817e153cb1c23f82e6
Risheit <80658104+Risheit@users.noreply.github.com>
2022-12-03 17:45:54 -0500

    Implement makeTimeline

--- app/src/main/java/com/models/Timeline.java
+++ app/src/main/java/com/models/Timeline.java
@@ -7,11 +7,6 @@ import com.models.course.Course;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO: Ask for Session to implement Comparable -- I want to sort the list/keep it sorted for ease of display
-// TODO: Ask for method that takes takes a session (Pair<String, int>) and increases it to the next session
-// TODO: A containsAnyOf method that returns true if any course in a list of courses is in a Session
-// TODO: SessionalDates is the possible seasons right?
-
 public class Timeline {
     List<Session> timeline;
 
@@ -30,14 +25,6 @@ public class Timeline {
         NOTE: This method currently allows for inf courses in one semester.
                     Do we want cap on courses per sem?
 
-
-        generateTimeline(coursesTaken, coursesPlanned):
-            i := 0
-            timeline t;
-            WHILE i < coursesPlanned.length:
-                placeCourseOnTimeline(coursesPlanned[i], coursesTaken, t)
-            ENDWHILE
-
          */
 
         Timeline timeline = new Timeline();
@@ -50,33 +37,11 @@ public class Timeline {
     }
 
     public void placeCourseOnTimeline(List<Course> coursesTaken, Course coursePlanned) {
-        /*
-         placeCourseOnTimeline(course, coursesTaken, timeline):
-            prereqs := the list of prereqs of course
-            sessions := the list of sessions that the course is available in
-
-            IF course not on timeline:
-                IF prereqs is empty OR all prereqs of course checked:
-                    i := Fall 2023 (Starting Session)
-                    WHILE true:
-                        IF i contains prereq OR sessions does not contain i,
-                            Increase i by the length of one session.
-                        ELSE
-                            Add the course to the timeline during session i.
-                            BREAK
-                    ENDWHILE
-                ELSE:
-                    i := 0
-                    WHILE i < prereqs:
-                        placeCourseOnTimeline(prereqs[i], coursesTaken, timeline)
-                    ENDWHILE
-                ENDIF
-            ENDIF
-         */
-
         List<Course> prereqs = coursePlanned.getPrerequisites();
-        List<String> sessions = coursePlanned.getSessionalDates();
+        List<String> seasonsAllowed = coursePlanned.getSessionalDates();
 
+        Pair<String, Integer> i = Session.getCurrentSession();
+        Session iSession = findSessionByName(i.first, i.second);
 
         if (containsCourse(coursePlanned)) {
             return;
@@ -86,11 +51,11 @@ public class Timeline {
             placeCourseOnTimeline(coursesTaken, prereq);
         }
 
-        Pair<String, Integer> i = Session.getCurrentSession();
         do {
-            if (/*findSessionByName(i.first, i.second).containsAnyOf(prereqs)
-                ||*/ sessions.contains(i.first)) {
-                // Increase i to next session
+            if ((iSession != null && iSession.anyCoursesInSession(prereqs))
+                || !seasonsAllowed.contains(i.first)) {
+                i = Session.moveToNextSession(i);
+                iSession = findSessionByName(i.first, i.second);
             } else {
                 addToSession(i.first, i.second, coursePlanned);
                 break;
@@ -141,5 +106,7 @@ public class Timeline {
         } else {
             foundSession.addToCourseList(course);
         }
+
+        timeline.sort(null);
     }
 }
