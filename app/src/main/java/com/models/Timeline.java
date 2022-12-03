8d85bb01ebe46874a8c241c30b428daeb9c9c1d9
Risheit <80658104+Risheit@users.noreply.github.com>
2022-12-03 01:09:04 -0500

    Formalize pseudocode more

--- app/src/main/java/com/models/Timeline.java
+++ app/src/main/java/com/models/Timeline.java
@@ -2,6 +2,7 @@ package com.models;
 
 import com.models.course.Course;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Timeline {
@@ -9,28 +10,48 @@ public class Timeline {
                                                  List<Course> coursesPlanned) {
         /*
 
-            Pick coursesPlanned[i], 0 <= i <= coursesPlanned.length.
-            IF course not on timeline: (*)
-                IF prereqs is empty OR all prereqs of course checked:
-                    Let [s] be the sessions the course is being held during.
-                    Begin with [i] = Fall 2023 (Starting Session)
-                        IF [i] contains prereq of course or [s] does not contain [i],
-                            Increase [i] by one session.
-                        ELSE
-                            Add the course to the timeline during session [i].
-
-                    NOTE: This allows for inf courses in one semester.
+        NOTE: This method currently allows for inf courses in one semester.
                     Do we want cap on courses per sem?
 
-                ELSE:
-                    Pick prereqs[i], 0 <= i <= prereqs.length.
-                    Move to [line *] with the chosen prereq course.
-                ENDIF
-            ENDIF
+
+        generateTimeline(coursesTaken, coursesPlanned):
+            i := 0
+            timeline t;
+            WHILE i < coursesPlanned.length:
+                placeCourseOnTimeline(coursesPlanned[i], coursesTaken, t)
+            ENDWHILE
 
          */
 
+        List<Session> timeline = new ArrayList<>();
+
 
         return null;
+    }
+
+    private static void placeCourseOnTimeline(List<Course> coursesTaken, Course coursePlanned,
+                                              List<Session> timeline) {
+        /*
+         placeCourseOnTimeline(course, coursesTaken, timeline):
+            prereqs := the list of prereqs of course
+            sessions := the list of sessions that the course is available in
+
+            IF course not on timeline:
+                IF prereqs is empty OR all prereqs of course checked:
+                    i := Fall 2023 (Starting Session)
+                    WHILE true:
+                        IF i contains prereq OR sessions does not contain i,
+                            Increase i by the length of one session.
+                        ELSE
+                            Add the course to the timeline during session i.
+                    ENDWHILE
+                ELSE:
+                    i := 0
+                    WHILE i < prereqs:
+                        placeCourseOnTimeline(prereqs[i], coursesTaken, timeline)
+                    ENDWHILE
+                ENDIF
+            ENDIF
+         */
     }
 }
