5661df789091ebdba70d5de62bc366291985a45b
Risheit <80658104+Risheit@users.noreply.github.com>
2022-12-03 00:24:36 -0500

    Use Lists instead of Sets/Maps and prevent duplicate items when inserting.

--- app/src/main/java/com/models/users/User.java
+++ app/src/main/java/com/models/users/User.java
@@ -1,27 +1,20 @@
 package com.models.users;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
     private String type;
     private String name;
     private String email;
     private String password;
-    private Map<String, String> courseCodesTaken;  // Key and value are identical
-    private Map<String, String> courseCodesPlanned; // Key and value are identical
+    private List<String> courseCodesTaken;
+    private List<String> courseCodesPlanned;
 
-    private final String dummyCourseTaken = "dTaken";
-    private final String dummyCoursePlanned = "dPlan";
 
     public User() {
-        this.courseCodesTaken = new HashMap<>();
-        courseCodesTaken.put(dummyCourseTaken, dummyCourseTaken);
-
-        this.courseCodesPlanned = new HashMap<>();
-        courseCodesPlanned.put(dummyCourseTaken, dummyCoursePlanned);
+        courseCodesTaken = new ArrayList<>();
+        courseCodesPlanned = new ArrayList<>();
     }
 
     /**
@@ -38,44 +31,8 @@ public class User {
         this.email = email;
         this.password = password;
 
-        this.courseCodesTaken = new HashMap<>();
-        courseCodesTaken.put(dummyCourseTaken, dummyCourseTaken);
-
-        this.courseCodesPlanned = new HashMap<>();
-        courseCodesPlanned.put(dummyCourseTaken, dummyCoursePlanned);
-    }
-
-    /**
-     * This constructor returns an instance of a User
-     * @param type  Indicates whether a User is a student or admin
-     * @param name  The full name of the user
-     * @param email  The email that the user's account is linked to
-     * @param password  The password of the user's account
-     * @param courseCodesTaken  The courses that the user has already represented by its code
-     * @param courseCodesPlanned The courses that the user plans to take represented by its code
-     */
-
-    public User(String type, String name, String email, String password, Set<String> courseCodesTaken,
-                Set<String> courseCodesPlanned) {
-        this.type = type;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.setCourseCodesTakenFromSet(courseCodesTaken);
-        this.setCourseCodesPlannedFromSet(courseCodesPlanned);
-    }
-
-    private static Set<String> mapToSet(Map<String, String> map) {
-        return new HashSet<>(map.keySet());
-    }
-
-    private static Map<String, String> setToMap(Set<String> set) {
-        Map<String, String> map = new HashMap<>();
-        for (String entry : set) {
-            map.put(entry, entry);
-        }
-
-        return map;
+        this.courseCodesTaken = new ArrayList<>();
+        this.courseCodesPlanned = new ArrayList<>();
     }
 
     /**
@@ -152,127 +109,63 @@ public class User {
 
     /**
      * This method retrieves the calling object's (an instance of User)
-     * taken courses. Prefer the use of {@code getCourseCodesTakenAsSet} instead, as the
-     * retrieved data from this method is unfiltered and contains data intended for internal use
-     * that may cause issues in other code.
-     *
-     * This method exists to allow compatibility with Firebase.
-     * @return A set of codes representing the courses the user has taken.
+     * taken courses.
+     * @return A list of codes representing the courses the user has taken.
      */
 
-    public Map<String, String> getCourseCodesTaken() {
+    public List<String> getCourseCodesTaken() {
         return courseCodesTaken;
     }
 
     /**
-     * This method retrieves the calling object's (an instance of User)
-     * taken courses as a set.
-     * @return A set of codes representing the courses the user has taken.
+     * This method modifies the calling object's (an instance of User) planned courses.
+     * @param courseCodesTaken  The codes of the courses the user plans to take
      */
 
-    public Set<String> getCourseCodesTakenAsSet() {
-        Set<String> set = mapToSet(courseCodesTaken);
-        set.remove(dummyCourseTaken);
-        return set;
-    }
-
-    public void setCourseCodesTaken(Map<String, String> courseCodesTaken) {
+    public void setCourseCodesTaken(ArrayList<String> courseCodesTaken) {
         this.courseCodesTaken = courseCodesTaken;
-        this.courseCodesTaken.put(dummyCourseTaken, dummyCourseTaken);
     }
 
     /**
-     *  This method modifies the calling object's (an instance of User) set of courses taken from a
-     *  given set.
-     * @param courseCodesTaken  The new courses taken for the user
-     */
-
-    public void setCourseCodesTakenFromSet(Set<String> courseCodesTaken) {
-        Map<String, String> map = setToMap(courseCodesTaken);
-        map.put(dummyCourseTaken, dummyCourseTaken);
-        this.courseCodesTaken = map;
-    }
-
-    /**
-     * This method adds a course to the set containing the courses this user has taken.
+     * This method adds a course to the list containing the courses this user has taken if that
+     * course is not already in the list.
      * @param courseCode  The code of the course to be added
-     * @return True if the course is added, false if the user has already taken this course.
+     * @return True iff the course is added to the list.
      */
 
     public boolean addTakenCourse(String courseCode) {
-        Set<String> courseCodesTaken = this.getCourseCodesTakenAsSet();
-        boolean result = courseCodesTaken.add(courseCode);
-        if (result) {
-            setCourseCodesTakenFromSet(courseCodesTaken);
-        }
-
-        return result;
+        return !courseCodesTaken.contains(courseCode) && courseCodesTaken.add(courseCode);
     }
 
     /**
-     * This method removes a course from the set containing the courses this user has taken.
+     * This method removes a course from the list containing the courses this user has taken.
      * @param courseCode  The code of the course to be added
      * @return True if the course is removed successfully, false otherwise.
      */
 
     public boolean removeTakenCourse(String courseCode) {
-        Set<String> courseCodesTaken = this.getCourseCodesTakenAsSet();
-        boolean result = courseCodesTaken.remove(courseCode);
-        if (result) {
-            setCourseCodesTakenFromSet(courseCodesTaken);
-        }
-
-        return result;
-    }
-
-    /**
-     * This method retrieves the calling object's (an instance of User)
-     * planned courses. Prefer the use of {@code getCourseCodesPlannedAsSet} instead, as the
-     * retrieved data from this method is unfiltered and contains data intended for internal use
-     * that may cause issues in other code.
-     *
-     * This method exists to allow compatibility with Firebase.
-     * @return A set of codes representing the courses the user has planned.
-     */
-
-
-    public Map<String, String> getCourseCodesPlanned() {
-        return courseCodesPlanned;
+        return courseCodesTaken.remove(courseCode);
     }
 
     /**
      * This method retrieves the calling object's (an instance of User)
      * planned courses.
-     * @return A set of codes representing the courses the user has planned.
+     * @return A list of codes representing the courses the user has planned.
      */
 
-    public Set<String> getCourseCodesPlannedAsSet() {
-        Set<String> set = mapToSet(courseCodesPlanned);
-        set.remove(dummyCoursePlanned);
-        return set;
+    public List<String> getCourseCodesPlanned() {
+        return courseCodesPlanned;
     }
 
     /**
      *  This method modifies the calling object's (an instance of User) set of courses planned.
-     * @param courseCodesPlanned  The new courses planned for the user
+     * @param courseCodesPlanned  The course codes representing new courses planned for the user
      */
 
-    public void setCourseCodesPlanned(Map<String, String> courseCodesPlanned) {
+    public void setCourseCodesPlanned(ArrayList<String> courseCodesPlanned) {
         this.courseCodesPlanned = courseCodesPlanned;
-        this.courseCodesPlanned.put(dummyCoursePlanned, dummyCoursePlanned);
     }
 
-    /**
-     *  This method modifies the calling object's (an instance of User) set of courses planned from
-     *  a given set.
-     * @param courseCodesPlanned  The new courses planned for the user
-     */
-
-    public void setCourseCodesPlannedFromSet(Set<String> courseCodesPlanned) {
-        Map<String, String> map = setToMap(courseCodesPlanned);
-        map.put(dummyCoursePlanned, dummyCoursePlanned);
-        this.courseCodesPlanned = map;
-    }
 
     /**
      * This method adds a course to the set containing the courses this user has planned to take.
@@ -282,13 +175,7 @@ public class User {
      */
 
     public boolean addPlannedCourse(String courseCode) {
-        Set<String> courseCodesPlanned = this.getCourseCodesPlannedAsSet();
-        boolean result = courseCodesPlanned.add(courseCode);
-        if (result) {
-            setCourseCodesPlannedFromSet(courseCodesPlanned);
-        }
-
-        return result;
+        return !courseCodesPlanned.contains(courseCode) && courseCodesPlanned.add(courseCode);
     }
 
     /**
@@ -299,12 +186,7 @@ public class User {
      */
 
     public boolean removePlannedCourse(String courseCode) {
-        Set<String> courseCodesPlanned = this.getCourseCodesPlannedAsSet();
-        boolean result = courseCodesPlanned.remove(courseCode);
-        if (result) {
-            setCourseCodesPlannedFromSet(courseCodesPlanned);
-        }
+        return courseCodesPlanned.remove(courseCode);
 
-        return result;
     }
 }
