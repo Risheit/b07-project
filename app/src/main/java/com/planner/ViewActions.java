1fc4458d4b6ef94dc4cbdf4d2c38a8d9f766ca58
Risheit <80658104+Risheit@users.noreply.github.com>
2022-12-03 20:08:10 -0500

    Make ViewActions abstract

--- app/src/main/java/com/planner/ViewActions.java
+++ app/src/main/java/com/planner/ViewActions.java
@@ -3,7 +3,7 @@ package com.planner;
 import android.content.Context;
 import android.widget.Toast;
 
-public class ViewActions {
+public abstract class ViewActions {
     public static void displayErrorNotification(Context callingActivity, String errorName) {
         Toast.makeText(callingActivity, errorName,
                 Toast.LENGTH_SHORT).show();
